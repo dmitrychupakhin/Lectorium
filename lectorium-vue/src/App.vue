3e5c102fd4beb1ea2e758a373fe36d36d8a87aaa
Dmitry Chupakhin <chupahkin@gmail.com>
2024-03-15 15:07:29 +0300

    Update uploading files

--- lectorium-vue/src/App.vue
+++ lectorium-vue/src/App.vue
@@ -1,42 +1,56 @@
 <template>
   <div>
-
     <input type="file" @change="handleFileUpLoad" />
-    <button click="upLoadFile">Загрузить</button>
+    <button @click="uploadFile">Загрузить</button>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   data() {
     return {
-      file: null
+      file: null,
     };
   },
   methods: {
     handleFileUpload(event) {
       this.file = event.target.files[0];
     },
-    uploadFile() {
-      const formData = new FormData();
-      formData.append('file', this.file);
+    async uploadFile() {
+      console.log("123");
+      try {
+        await axios.post("http://127.0.0.1:8000/api/v1/lecture/", {
+            title: "Лекция",
+            content: "Content"
+        });
+      } catch (e) {
+        console.log(e);
+        alert("Ошибка");
+      } finally {
+        this.isPostsLoading = false;
+      }
+      /* const formData = new FormData();
+      formData.append("file", this.file);
 
-      axios.post('http://example.com/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-        .then(response => {
-          console.log('File uploaded successfully:', response.data);
+      axios
+        .post("http://127.0.0.1:8000/api/v1/lecture/", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            title: "Лекция",
+            file: file,
+          },
+        })
+        .then((response) => {
+          console.log("File uploaded successfully:", response.data);
           // Дополнительная обработка ответа, если требуется
         })
-        .catch(error => {
-          console.error('Error uploading file:', error);
-        });
-    }
+        .catch((error) => {
+          console.error("Error uploading file:", error);
+        }); */
+    },
   },
-}
+};
 </script>
 
 <style></style>

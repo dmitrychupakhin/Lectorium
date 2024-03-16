4cf6d18e1fb15cccac22e1f01788b0a1a7d48892
vituyac <skisterev78@mail.ru>
2024-03-16 12:12:44 +0300

    edit model Lecture

--- lectorium-django/lecture/models.py
+++ lectorium-django/lecture/models.py
@@ -4,5 +4,9 @@ import os
 class Lecture(models.Model):
     title = models.TextField()
     content = models.FileField(upload_to='media')
+    faculty = models.TextField()
+    lecturer = models.TextField()
+    cource = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     

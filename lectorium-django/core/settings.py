9808e2af7bf0e03d78de95400c32d5b7edaf0591
Dmitry Chupakhin <chupahkin@gmail.com>
2024-03-16 17:43:19 +0300

    Vue + Django = oh ye yee oh no

--- lectorium-django/core/settings.py
+++ lectorium-django/core/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:80',  # Добавьте здесь ваш домен
+    'http://localhost',
+    'http://127.0.0.1:80',
     'http://127.0.0.1:8000',
 ]

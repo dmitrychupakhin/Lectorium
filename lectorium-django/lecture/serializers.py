3cbcbbfb5e8e72b0e105720285be98d4a9323b1e
vituyac <skisterev78@mail.ru>
2024-03-17 11:14:51 +0300

    +serializer

--- lectorium-django/lecture/serializers.py
+++ lectorium-django/lecture/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 from account.serializers import *
+from account.models import Account
 
 class FacultySerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,11 +16,19 @@ class CourseSerializer(serializers.ModelSerializer):
         #read_only_fields = ('id',)
         
 class LectureSerializer(serializers.ModelSerializer):
+    faculty = serializers.CharField(source='faculty.title')
+    cource = serializers.CharField(source='cource.title')
+    lecturer = serializers.SerializerMethodField()
     class Meta:
         model = Lecture
         fields = ('id', 'title_lect', 'faculty','lecturer','cource','content')
         read_only_fields = ('id',)
-        
+    
+    def get_lecturer(self, obj):
+        if obj.lecturer:
+            return f"{obj.lecturer.first_name} {obj.lecturer.last_name}"
+        else:
+            return None
     
         
         
